c44ea9bee9d08472adf5e48753803b28cde697ac
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-09-20 21:17:45 +0500

    sidebar completed

--- app/(root)/components/sidebar.tsx
+++ app/(root)/components/sidebar.tsx
@@ -1,12 +1,15 @@
 "use client"
 
+import React from 'react'
+import { usePathname, useRouter } from "next/navigation"
+
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import { UserButton, useUser } from '@clerk/nextjs'
-import { PersonStanding, Plus, Settings, Zap } from 'lucide-react'
-import React from 'react'
-import { usePathname } from "next/navigation"
-import { useTheme } from 'next-themes'
+
+import { Plus, Settings, Zap } from 'lucide-react'
+import { ModeToggle } from './mode-toggle'
+
 
 const Sidebar = () => {
 
@@ -27,30 +30,38 @@ const Sidebar = () => {
 
     const { user } = useUser();
     const pathname = usePathname();
-    const { setTheme } = useTheme();
+    const router = useRouter();
+
+    const onClick = (href: string) => {
+        router.push(href);
+    }
 
     return (
         <div className='flex flex-col items-center justify-center h-full w-20 gap-y-3 text-sm'>
             {
                 routes.map((route) =>
                     <div key={route.title}
-                        className={cn(`group p-2 w-full cursor-pointer rounded-lg hover:bg-slate-300
-                    text-gray-600 hover:text-gray-800 transition`,
+                        className={cn(`group p-2 w-full cursor-pointer rounded-lg hover:bg-primary/60
+                    text-primary/80 hover:text-primary transition`,
                             pathname === route.href && "bg-slate-300 text-gray-600"
                         )}
                     >
-                        <div className='flex flex-col gap-y-2 items-center justify-center'>
+                        <div className='flex flex-col gap-y-2 items-center justify-center'
+                            onClick={() => onClick(route.href)}
+                        >
                             <route.icon />
                             {route.title}
                         </div>
                     </div>
                 )
             }
-            <div className='p-2 w-full hover:bg-slate text-gray-600 hover:text-gray-800'>
-
+            <div className='p-2 w-full cursor-pointer rounded-lg hover:bg-primary/60
+                    text-primary/80 hover:text-primary transition flex items-center justify-center'>
+                <ModeToggle />
             </div>
-            <div className='p-2 w-full hover:bg-slate-300 flex flex-col gap-y-2 items-center justify-center
-            text-gray-600 hover:text-gray-800'>
+            <div className='p-2 w-full cursor-pointer rounded-lg hover:bg-primary/60
+            text-primary/80 hover:text-primary transition flex flex-col items-center 
+            justify-center gap-y-2'>
                 <UserButton />
                 {user?.firstName}
             </div>

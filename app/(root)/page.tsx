a910b3d5eeb14eada3129a6f7fec2b96790d858b
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-09-20 13:41:05 +0500

    authentication completed

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+type Props = {}
+
+const Home = () => {
+    return (
+        <div>
+            <UserButton />
+        </div>
+    )
+}
+
+export default Home

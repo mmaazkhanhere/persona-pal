4829d4ccebadd055edb9f8f943251554b48e6682
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-09-21 13:16:17 +0500

    prisma installed

--- app/(root)/bot/[botId]/page.tsx
+++ app/(root)/bot/[botId]/page.tsx
@@ -1,6 +1,28 @@
+import prismadb from '@/lib/prismadb';
+import { auth, redirectToSignIn } from '@clerk/nextjs'
 import React from 'react'
 
-const Bot = () => {
+interface PalProps {
+    params: {
+        palId: string
+    }
+}
+
+const Bot = async ({ params }: PalProps) => {
+
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirectToSignIn();
+    }
+
+    const initalData = await prismadb.pal.findUnique({
+        where: {
+            id: params.palId,
+            userId //only user who created it change the b
+        }
+    })
+
     return (
         <div>Bot</div>
     )
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/toaster'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -23,6 +24,7 @@ export default function RootLayout({
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
           <body className={inter.className}>
             {children}
+            <Toaster />
           </body>
         </ThemeProvider>
       </html>
